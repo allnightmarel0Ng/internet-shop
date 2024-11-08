b7dfb7f896a406d4e32020968b07561f2d8764f1
allnightmarel0Ng <den.vasiliev65@gmail.com>
2024-11-08 22:37:03 +0300

    pep8 linted

--- cmd/authorization/main.py
+++ cmd/authorization/main.py
@@ -12,14 +12,17 @@ from internal.app.authorization.handler import AuthorizationHandler
 
 if __name__ == '__main__':
     config = load_config()
-    
-    db = Database(f"postgresql://{config.postgres_user}:{config.postgres_password}@postgres:{config.postgres_port}/{config.postgres_db}")
-    
+
+    db = Database(
+        f"postgresql://{config.postgres_user}:{config.postgres_password}@postgres:{config.postgres_port}/{config.postgres_db}")
+
     shop_repository = ShopRepository(db)
     user_repository = UserRepository(db)
 
-    authorization_repository = AuthorizationRepository(shop_repository, user_repository)
-    authorization_use_case = AuthorizationUseCase(authorization_repository, config.jwt_secret_key)
+    authorization_repository = AuthorizationRepository(
+        shop_repository, user_repository)
+    authorization_use_case = AuthorizationUseCase(
+        authorization_repository, config.jwt_secret_key)
     authorization_handler = AuthorizationHandler(authorization_use_case)
 
     asyncio.run(authorization_handler.start(int(config.authorization_ws_port)))
