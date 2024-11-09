2bcbaa6e590c554905d4f3500538056fc13b279a
allnightmarel0Ng <den.vasiliev65@gmail.com>
2024-11-09 22:33:06 +0300

    init.sql -> ddl.sql

--- migrations/ddl.sql
+++ migrations/ddl.sql
@@ -16,7 +16,8 @@ CREATE TABLE public.users (
     id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(50) NOT NULL,
     login VARCHAR (30) NOT NULL,
-    password VARCHAR(30) NOT NULL
+    password VARCHAR(30) NOT NULL,
+    balance MONEY NOT NULL
 );
 
 CREATE TABLE public.categories (
@@ -27,6 +28,7 @@ CREATE TABLE public.categories (
 CREATE TABLE public.products (
     id SERIAL PRIMARY KEY NOT NULL,
     category_id INT REFERENCES public.categories(id) ON DELETE SET NULL,
+    shop_id INT REFERENCES public.shops(id) ON DELETE SET NULL,
     name VARCHAR(50) NOT NULL,
     price MONEY NOT NULL,
     description TEXT
@@ -36,7 +38,8 @@ CREATE TABLE public.paychecks (
     id SERIAL PRIMARY KEY NOT NULL,
     paycheck_id INT NOT NULL,
     user_id INT REFERENCES public.users(id) ON DELETE SET NULL,
-    product_id INT REFERENCES public.products(id) ON DELETE SET NULL
+    product_id INT REFERENCES public.products(id) ON DELETE SET NULL,
+    creation_datetime DATETIME NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE public.shopping_carts (
