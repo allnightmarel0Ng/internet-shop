994751577c0e98178e915cda07732d956e4dd64d
mari <kese@WIN-291JKESS4HP>
2024-12-12 23:07:16 +0300

    %query%query%

--- internal/app/search/usecase.py
+++ internal/app/search/usecase.py
@@ -3,4 +3,12 @@ class SearchUseCase:
         self.repository = repository
 
     async def search(self, query: str) -> dict:
-        return self.repository.search(query)
+
+        if not query or not isinstance(query, str) or len(query.strip()) == 0:
+            return []
+
+        query_words = query.strip().split()
+
+        query_like_conditions = [f"%{word}%" for word in query_words]
+
+        return await self.repository.search(query_like_conditions)
